3655b203678ab08429d1238cc6a11d358b2b6fc6
Damjan Radev <radevdamjan@gmail.com>
2020-03-14 11:34:45 +0100

    Added responsivness properties to theme

--- components/Page/index.js
+++ components/Page/index.js
@@ -1,28 +1,28 @@
-import React, { Component } from "react";
-import styled, { ThemeProvider, injectGlobal } from "styled-components";
-import Meta from "../Meta";
-import Header from "../Header";
-import Footer from "../Footer"
-import {withRouter} from "next/router"
+import React, { Component } from 'react';
+import styled, { ThemeProvider, injectGlobal, css } from 'styled-components';
+import Meta from '../Meta';
+import Header from '../Header';
+import Footer from '../Footer';
+import { withRouter } from 'next/router';
 
 const theme = {
-  red: "#FF5964",
-  whiteGray:"#f2f2f2",
-  black: "#393939",
-  grey: "#3A3A3A",
-  blue: "#2F5DA8",
-  lightgrey: "#E1E1E1",
-  offWhite: "#EDEDED",
-  maxWidth: "1000px",
-  bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
+  red: '#FF5964',
+  whiteGray: '#f2f2f2',
+  black: '#393939',
+  grey: '#3A3A3A',
+  blue: '#2F5DA8',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+  containerMaxHeight: 'calc(100vh - 84px)'
 };
 
 const StyledPage = styled.div`
   background: white;
   color: ${props => props.theme.black};
-  padding-top: ${props => props.router.route !== "/" && "100px"};
+  padding-top: ${props => props.router.route !== '/' && '100px'};
 `;
-
 
 injectGlobal`
   @font-face {
@@ -31,6 +31,12 @@ injectGlobal`
     font-weight: normal;
     font-style: normal;
   }
+  @font-face {
+    font-family: 'Montserrat';
+    
+    @import url('https://fonts.googleapis.com/css?family=Montserrat:200,200i,300,300i,400,600,600i&display=swap');
+  }
+  
   html {
     box-sizing: border-box;
     font-size: 10px;
@@ -43,7 +49,15 @@ injectGlobal`
     margin: 0;
     font-size: 1.5rem;
     line-height: 2;
-    font-family: 'radnika_next';
+    font-family: 'Montseraat';
+  }
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0;
   }
   p {
     margin: 0;
@@ -61,16 +75,15 @@ injectGlobal`
 
 class Page extends Component {
   render() {
-    
-      return (
-        <ThemeProvider theme={theme}>
-          <StyledPage router={this.props.router}>
-            <Meta />
-            <Header />
-            {this.props.children}
-            <Footer />
-          </StyledPage>
-        </ThemeProvider>
+    return (
+      <ThemeProvider theme={theme}>
+        <StyledPage router={this.props.router}>
+          <Meta />
+          <Header />
+          {this.props.children}
+          <Footer />
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
