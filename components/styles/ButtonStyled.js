40f11c852bd921ae71ee84bf38ea13d208c1147f
Damjan Radev <radevdamjan@gmail.com>
2020-06-02 22:21:23 +0200

    Added animation for homepage buttons

--- components/styles/ButtonStyled.js
+++ components/styles/ButtonStyled.js
@@ -1,19 +1,39 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const buttonClick = keyframes`
+  0% {
+    font-size: 1.5rem; 
+  }
+  50% {
+    font-size: 1.3rem;
+  }
+  100% {
+    font-size: 1.5rem;
+  }
+`;
 
 const ButtonStyled = styled.button`
   outline: none;
   margin-top: 2rem;
-  width: 70%;
+  width: 90%;
   padding: 2rem;
-  background: ${props => props.background || 'transparent'};
-  border: 3px solid ${props => props.border || 'transparent'};
-  color: ${props => props.color || props.theme.whiteGray};
+  background: ${(props) => props.btnColor || 'transparent'};
+  border: 3px solid ${(props) => props.border || 'transparent'};
+  color: ${(props) => props.color || props.theme.whiteGray};
   border-radius: 5px;
   font-size: 1.5rem;
   text-transform: uppercase;
 
+  transition: background 0.2s;
+  cursor: pointer;
+
   &:hover {
-    background: ${props => props.btnColor};
+    background: ${(props) => props.hover};
+  }
+
+  &:active,
+  &:focus {
+    animation: ${buttonClick} 0.3s ease-out;
   }
 `;
 
