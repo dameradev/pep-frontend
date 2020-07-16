d050aa104b7a760974e997f6c9d51defa2ab2062
Damjan Radev <radevdamjan@gmail.com>
2020-07-16 18:26:19 +0200

    Material ui icons

--- components/Meta/index.js
+++ components/Meta/index.js
@@ -11,6 +11,7 @@ const Meta = (props) => {
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet"
       ></link>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       <script
         type="text/javascript"
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB-ufkApsYu5oKF4y4afg5ZCWbaaUyjTYY&libraries=places"
