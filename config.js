369da528374ae69392b136005b65d03683474bf4
Damjan Radev <radevdamjan@gmail.com>
2020-11-07 14:51:31 +0100

    Added heroku endpoint

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
+export const productionEndpoint = `https://pep-eu.herokuapp.com`;
 export const perPage = 4;
 export const projectTypes = [
   { value: 'ESC', label: 'ESC' },
