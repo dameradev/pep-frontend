5708e53b1b6dc691ecf3ae772b36448e09132162
Damjan Radev <radevdamjan@gmail.com>
2020-11-19 12:53:46 +0100

    Refactor signin component

--- components/Signin/index.js
+++ components/Signin/index.js
@@ -1,101 +1,84 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 import { Mutation } from 'react-apollo';
-import gql from 'graphql-tag';
 
 import Form from '../styles/Form';
-import ButtonStyled from '../styles/ButtonStyled';
 import Center from '../styles/Center';
-
 import ErrorMessage from '../styles/ErrorMessage';
-import { CURRENT_USER_QUERY } from '../User';
 
-const SIGNIN_MUTATION = gql`
-  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
-    signin(email: $email, password: $password) {
-      user {
-        name
-        age
-        id
-        email
-      }
-      token
-    }
-  }
-`;
+import { CURRENT_USER_QUERY } from '../../lib/queries';
+import { SIGNIN_MUTATION } from '../../lib/mutations';
+import useForm from '../../lib/useForm';
 
-class Signin extends Component {
-  state = { email: '', password: '' };
+const Signin = () => {
+  const { values: { email, password } = {}, values, updateValue } = useForm({
+    email: '',
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+    password: '',
+  });
 
-  render() {
-    const { email, password } = this.state;
-    return (
-      <Mutation
-        mutation={SIGNIN_MUTATION}
-        variables={this.state}
-        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
-      >
-        {(signin, { error, loading }) => (
-          <Center>
-            <Form
-              method="post"
-              onSubmit={async (e) => {
-                e.preventDefault();
-                const { data } = await signin();
-                localStorage.setItem('token', data.signin.token);
+  return (
+    <Mutation
+      mutation={SIGNIN_MUTATION}
+      variables={{ email, password }}
+      refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+    >
+      {(signin, { error, loading }) => (
+        <Center>
+          <Form
+            method="post"
+            onSubmit={async (e) => {
+              e.preventDefault();
+              const { data } = await signin();
+              localStorage.setItem('token', data.signin.token);
 
-                Router.push('/');
+              Router.push('/');
 
-                this.setState({ email: '', password: '' });
-              }}
-            >
-              <h2>Login to an exising account</h2>
-              <fieldset disabled={loading} aria-busy={loading}></fieldset>
-              <ErrorMessage error={error} />
+              // this.setState({ email: '', password: '' });
+            }}
+          >
+            <h2>Login to an exising account</h2>
+            <fieldset disabled={loading} aria-busy={loading}></fieldset>
+            <ErrorMessage error={error} />
 
-              <label htmlFor="email">
-                <input
-                  name="email"
-                  type="email"
-                  id="email"
-                  value={email}
-                  placeholder="Please enter your email"
-                  onChange={this.handleChange}
-                />
-              </label>
+            <label htmlFor="email">
+              <input
+                name="email"
+                type="email"
+                id="email"
+                value={email}
+                placeholder="Please enter your email"
+                onChange={updateValue}
+              />
+            </label>
 
-              <label htmlFor="password">
-                <input
-                  name="password"
-                  type="password"
-                  id="password"
-                  value={password}
-                  placeholder="Please enter your password"
-                  onChange={this.handleChange}
-                />
-              </label>
-              <div className="links-container">
-                <Link href={{ pathname: '/auth', query: { path: 'register' } }}>
-                  <a>Don't have an account?</a>
-                </Link>
+            <label htmlFor="password">
+              <input
+                name="password"
+                type="password"
+                id="password"
+                value={password}
+                placeholder="Please enter your password"
+                onChange={updateValue}
+              />
+            </label>
+            <div className="links-container">
+              <Link href={{ pathname: '/auth', query: { path: 'register' } }}>
+                <a>Don't have an account?</a>
+              </Link>
 
-                <Link href={{ pathname: '/auth', query: { path: 'reset' } }}>
-                  <a>Forgot password?</a>
-                </Link>
-              </div>
+              <Link href={{ pathname: '/auth', query: { path: 'reset' } }}>
+                <a>Forgot password?</a>
+              </Link>
+            </div>
 
-              <button type="submit">Log in</button>
-            </Form>
-          </Center>
-        )}
-      </Mutation>
-    );
-  }
-}
+            <button type="submit">Log in</button>
+          </Form>
+        </Center>
+      )}
+    </Mutation>
+  );
+};
 
 export default Signin;
