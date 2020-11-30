581f999c8e8d7e7d031a16dea25db7af1ca3e496
Damjan Radev <radevdamjan@gmail.com>
2020-11-30 12:53:05 +0100

    Search filters styles

--- components/SearchPanel/styles.js
+++ components/SearchPanel/styles.js
@@ -7,22 +7,31 @@ export const SearchPanelStyles = styled.div`
   background-color: #fff;
   border-radius: 7px;
   min-height: 80vh;
-  position: relative;
-  /* padding: 20px; */
+  width: 30rem;
+  color: #6b6b6b;
 
   ${respondTo.tabletMini` 
     margin-top: 0;
     min-height: unset;
+    width: 100%;
   `}
 
   .Header {
-    border-bottom: 1px solid ${(props) => props.theme.borderColorPrimary};
-    padding: 2rem;
+    position: fixed;
+    width: 30rem;
+    ${respondTo.tabletMini` 
+      position: static;
+      width: 100%;
+    `}
 
     h3 {
       text-transform: uppercase;
+      border-bottom: 2px solid #95989d;
       font-size: 2.5rem;
       font-weight: 300;
+      width: 100%;
+      /* margin: 0 -2rem; */
+      padding: 2rem;
     }
   }
   .form-input {
@@ -34,15 +43,13 @@ export const SearchPanelStyles = styled.div`
     }
   }
   .filters {
-    margin-top: 2rem;
     padding: 2rem;
     display: grid;
     grid-gap: 2rem;
-    position: fixed;
     min-width: 25rem;
-
-    ${respondTo.tabletMini`
-      position: static
-    `}
+    button {
+      width: fit-content;
+      margin-left: auto;
+    }
   }
 `;
