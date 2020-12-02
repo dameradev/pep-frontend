8d323df72e0fef212d12d8b95fad5db567a53e0a
Damjan Radev <radevdamjan@gmail.com>
2020-12-02 22:00:37 +0100

    Added manifest.json

--- components/Meta/index.js
+++ components/Meta/index.js
@@ -7,21 +7,13 @@ const Meta = (props) => {
       <meta charSet="utf-8" />
       <link rel="shortcut icon" href="/static/favicon.png" />
       <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
-      {/* <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-        rel="stylesheet"
-      ></link> */}
+
       <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link rel="manifest" href="manifest.json" />
       <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,700;1,300&display=swap"
         rel="stylesheet"
       />
-      {/* <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" /> */}
-      {/* <script
-        type="text/javascript"
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB-ufkApsYu5oKF4y4afg5ZCWbaaUyjTYY&libraries=places"
-      ></script> */}
-      {/* <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>  */}
       <title>Platform for Erasmus Projects</title>
       <meta
         name="description"
