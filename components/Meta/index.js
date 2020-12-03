6d7c1278a549abc8e608824a0de602df1b63bd23
Damjan Radev <radevdamjan@gmail.com>
2020-12-03 17:29:35 +0100

    Prefetching fonts

--- components/Meta/index.js
+++ components/Meta/index.js
@@ -8,7 +8,8 @@ const Meta = (props) => {
       <link rel="shortcut icon" href="/static/favicon.png" />
       <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
 
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
       {/* <link rel="manifest" href="/manifest.json" /> */}
       <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,700;1,300&display=swap"
